67d05063b0a33adadd89dbc4a2276fab2ec2076e
HonbraDev <54026315+HonbraDev@users.noreply.github.com>
2021-04-01 18:52:52 +0200

    Fixed empty messages

--- src/utils/chatHandler.ts
+++ src/utils/chatHandler.ts
@@ -18,26 +18,28 @@ import {
 } from "./commands";
 
 const chatHandler = ({ userId, msg }: { userId: UUID; msg: Message }) => {
-  const text = tokensToString(msg.tokens);
+  if (msg.tokens.length > 0) {
+    const text = tokensToString(msg.tokens);
 
-  logger(`${msg.username} (${userId}): ${text}`);
+    logger(`${msg.username} (${userId}): ${text}`);
 
-  const commandInput: CommandInput = { wrapper, msg, userId };
-  if (["bbcd9b89-dd64-49f7-8612-49a333b6249b"].includes(userId)) return;
+    const commandInput: CommandInput = { wrapper, msg, userId };
+    if (["bbcd9b89-dd64-49f7-8612-49a333b6249b"].includes(userId)) return;
 
-  if (msg.tokens[0].v === "$help") help(commandInput);
-  if (msg.tokens[0].v === "$jam") jam(commandInput);
-  if (msg.tokens[0].v === "$monies") monies(commandInput);
-  if (msg.tokens[0].v === "$setdodge") setdodge(commandInput);
-  if (msg.tokens[0].v === "$pay") pay(commandInput);
-  if (msg.tokens[0].v === "$coinflip") coinflip(commandInput);
-  if (msg.tokens[0].v === "$reboot") reboot(commandInput);
-  if (msg.tokens[0].v === "$total") total(commandInput);
-  if (msg.tokens[0].v === "!slap") slap(commandInput);
-  if (msg.tokens[0].v === "!yeet") slap(commandInput);
-  if (msg.tokens[0].v === "$steal") steal(commandInput);
-  if (msg.tokens[0].v === "$speak") speak(commandInput);
-  if (msg.tokens[0].v === "$yeet") yeet(commandInput);
+    if (msg.tokens[0].v === "$help") help(commandInput);
+    if (msg.tokens[0].v === "$jam") jam(commandInput);
+    if (msg.tokens[0].v === "$monies") monies(commandInput);
+    if (msg.tokens[0].v === "$setdodge") setdodge(commandInput);
+    if (msg.tokens[0].v === "$pay") pay(commandInput);
+    if (msg.tokens[0].v === "$coinflip") coinflip(commandInput);
+    if (msg.tokens[0].v === "$reboot") reboot(commandInput);
+    if (msg.tokens[0].v === "$total") total(commandInput);
+    if (msg.tokens[0].v === "!slap") slap(commandInput);
+    if (msg.tokens[0].v === "!yeet") slap(commandInput);
+    if (msg.tokens[0].v === "$steal") steal(commandInput);
+    if (msg.tokens[0].v === "$speak") speak(commandInput);
+    if (msg.tokens[0].v === "$yeet") yeet(commandInput);
+  }
 };
 
 export default chatHandler;
