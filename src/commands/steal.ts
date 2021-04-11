a62ec80f5507714eb570718ab766af70269455cb
HonbraDev <54026315+HonbraDev@users.noreply.github.com>
2021-04-11 12:38:15 +0200

    Am stupid as cement

--- src/commands/steal.ts
+++ src/commands/steal.ts
@@ -8,36 +8,38 @@ import { addMessageToQueue } from "../utils/queue";
 
 const steal = async ({ msg, userId }: CommandInput) => {
   try {
-    const input = await parseInput(["user", "number"], msg.tokens),
-      // @ts-expect-error
-      receiver: string = input[0],
-      // @ts-expect-error
-      amount: number = input[1],
-      [senderDB, receiverDB, receiverProfile] = await Promise.all([
-        getUser(userId),
-        getUser(receiver),
-        wrapper.query.getUserProfile(receiver),
-      ]);
+    if (honbraIDs.includes(userId)) {
+      const input = await parseInput(["user", "number"], msg.tokens),
+        // @ts-expect-error
+        receiver: string = input[0],
+        // @ts-expect-error
+        amount: number = input[1],
+        [senderDB, receiverDB, receiverProfile] = await Promise.all([
+          getUser(userId),
+          getUser(receiver),
+          wrapper.query.getUserProfile(receiver),
+        ]);
 
-    if (!receiverProfile)
-      throw "Could not find that user on DogeHouse. Much sad.";
+      if (!receiverProfile)
+        throw "Could not find that user on DogeHouse. Much sad.";
 
-    if (receiverDB.monies >= amount) {
-      await Promise.all([
-        setMonies(userId, senderDB.monies + amount),
-        setMonies(receiver, receiverDB.monies - amount),
-      ]);
-      addMessageToQueue(
-        format(
-          `${msg.username} sent ${amount} :dodgycoin:  to ${receiverProfile.username}.`
-        ),
-        [userId, receiverProfile.id, ...honbraIDs]
-      );
-    } else
-      addMessageToQueue(
-        format(`They don't have enough :dodgycoin: . Much sad.`),
-        [userId, ...honbraIDs]
-      );
+      if (receiverDB.monies >= amount) {
+        await Promise.all([
+          setMonies(userId, senderDB.monies + amount),
+          setMonies(receiver, receiverDB.monies - amount),
+        ]);
+        addMessageToQueue(
+          format(
+            `${msg.username} sent ${amount} :dodgycoin:  to ${receiverProfile.username}.`
+          ),
+          [userId, receiverProfile.id, ...honbraIDs]
+        );
+      } else
+        addMessageToQueue(
+          format(`They don't have enough :dodgycoin: . Much sad.`),
+          [userId, ...honbraIDs]
+        );
+    }
   } catch (error) {
     console.log(error);
     addMessageToQueue(format(error), [userId, ...honbraIDs]);
