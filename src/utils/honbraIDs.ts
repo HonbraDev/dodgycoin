a32bd3ee27ad8e313be681ab42093ebfed1bca82
HonbraDev <54026315+HonbraDev@users.noreply.github.com>
2021-04-11 12:35:34 +0200

    Updated Honbra IDs, maybe I should implement it in the db

--- src/utils/honbraIDs.ts
+++ src/utils/honbraIDs.ts
@@ -3,5 +3,5 @@ export default [
   "897e1b31-9c1a-4e9c-8ae4-4385eca7ab2c",
   "8441e9d0-74a9-49ce-8c2f-e03c7dcb297c",
   "bd63e4dc-dbd5-4b9f-9668-8573189b52b3",
+  "d947951b-ed3c-4276-b3e2-fb0c013877a7",
 ];
-
